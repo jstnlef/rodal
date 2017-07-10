e41229634d8cf8df69c8b094611893956d8e6295
Isaac Oscar Gariano <u6331211@anu.edu.au>
2017-07-10 14:07:37 +1000

    Made using the wrong rustc version a compile time error

--- build.rs
+++ build.rs
@@ -16,6 +16,6 @@ extern crate rustc_version;
 
 fn main() {
     if rustc_version::version_meta().unwrap().commit_hash.unwrap() != "03fc9d622e0ea26a3d37f5ab030737fcca6928b9" {
-        println!("WARNING: expected rustc version 1.18.0 (03fc9d622 2017-06-06). (if you want to use a different one make sure the declarations and functions in dump_std correspond to your rustc's source code)")
+        panic!("expected rustc version 1.18.0 (03fc9d622 2017-06-06). (if you want to use a different one make sure the declarations and functions in dump_std correspond to your rustc's source code)")
     }
 }
