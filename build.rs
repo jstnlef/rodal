ad557574ff823db45acd14fe5c6db3b77a67a71f
Isaac Oscar Gariano <u6331211@anu.edu.au>
2017-07-23 12:20:43 +1000

    Updated version check

--- build.rs
+++ build.rs
@@ -15,7 +15,8 @@
 extern crate rustc_version;
 
 fn main() {
-    if rustc_version::version_meta().unwrap().commit_hash.unwrap() != "03fc9d622e0ea26a3d37f5ab030737fcca6928b9" {
-        panic!("expected rustc version 1.18.0 (03fc9d622 2017-06-06). (if you want to use a different one make sure the declarations and functions in dump_std correspond to your rustc's source code)")
+    let hash = rustc_version::version_meta().unwrap().commit_hash.unwrap();
+    if  hash != "03fc9d622e0ea26a3d37f5ab030737fcca6928b9" && hash != "0ade339411587887bf01bcfa2e9ae4414c8900d4" {
+        panic!("expected rustc version 1.18.0 (03fc9d622 2017-06-06) or 1.19.0 (0ade33941 2017-07-17) (if you want to use a different one make sure the declarations and functions in dump_std correspond to your rustc's source code)")
     }
 }
